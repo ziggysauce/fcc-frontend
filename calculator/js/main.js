9d372d6223b156c4ab9d6718f91c1676f167b84e
ziggysauce <dtn3423@gmail.com>
2017-12-07 01:41:30 -0500

    basic functionality

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -2,27 +2,58 @@
 
 // store variables
 const numButtons = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-const opsButtons = ['add', 'subtract', 'multiply', 'divide', 'percent', 'pos-neg', 'decimal'];
-let currentEquation = [];
+const opsButtons = ['add', 'subtract', 'multiply', 'divide'];
+let currentNums = [];
+let currentOp = '';
+let currentTotal = '';
+
+// Constantly update current total
+function updateTotal() {
+  if (currentNums.length === 2) {
+    if (currentOp === '+') {
+      currentTotal = Number(currentNums[0]) + Number(currentNums[1]);
+    } else if (currentOp === '-') {
+      currentTotal = Number(currentNums[0]) - Number(currentNums[1]);
+    } else if (currentOp === '*') {
+      currentTotal = Number(currentNums[0]) * Number(currentNums[1]);
+    } else if (currentOp === '/') {
+      currentTotal = Number(currentNums[0]) / Number(currentNums[1]);
+    }
+    currentNums[0] = currentTotal;
+    currentNums.pop();
+    console.log(currentNums);
+  }
+}
 
 // User clicks on a number
 // Number is shown in top and bottom window
 function clickNum(e) {
-  $('#bigInput')[0].innerHTML = e.target.innerHTML;
+  // Replace input if it's zero
+  if ($('#bigInput')[0].innerHTML === '0') {
+    $('#bigInput')[0].innerHTML = e.target.innerHTML;
+  } else {
+    // Get a number longer than 1 digit
+    $('#bigInput')[0].innerHTML += e.target.innerHTML;
+  }
   $('#smallInput')[0].innerHTML += e.target.innerHTML;
-  currentEquation.push(e.target.innerHTML);
 }
 
 
 // User clicks on an operation
 // Operation is shown in top window
 function clickOps(e) {
+  // Store previous number entered
+  currentNums.push($('#bigInput')[0].innerHTML);
+  updateTotal();
+
   if (e.target.innerHTML === 'x') {
+    $('#bigInput')[0].innerHTML = '';
     $('#smallInput')[0].innerHTML += ' * ';
-    currentEquation.push('*');
+    currentOp = '*';
   } else {
+    $('#bigInput')[0].innerHTML = '';
     $('#smallInput')[0].innerHTML += ` ${e.target.innerHTML} `;
-    currentEquation.push(e.target.innerHTML);
+    currentOp = e.target.innerHTML;
   }
 }
 
@@ -30,15 +61,24 @@ function clickOps(e) {
 // User clicks 'equal' button
 // Expression is calculated
 function clickEqual(e) {
-  console.log(e.target);
+  // Store previous number enetered
+  currentNums.push($('#bigInput')[0].innerHTML);
+  updateTotal();
+  // Display calculated total
+  $('#bigInput')[0].innerHTML = currentTotal;
+  $('#smallInput')[0].innerHTML = currentTotal;
+  // Clear stored numbers
+  currentNums = [];
 }
-
 
 // User clicks 'clear' button
 // Calculator window is emptied
 function clickClear(e) {
   $('#bigInput')[0].innerHTML = '0';
   $('#smallInput')[0].innerHTML = '';
+  currentNums = [];
+  currentOp = '';
+  currentTotal = '';
 }
 
 
@@ -68,9 +108,6 @@ function setupClicks() {
     clickClear(e);
   })
 }
-
-
-
 
 $(document).ready(() => {
   setupClicks();
