19a34ee581c8ad9a19028c0fac2ffde93c501102
ziggysauce <dtn3423@gmail.com>
2017-12-10 15:28:17 -0500

    add inc dec button fuctionality

--- pomodoro-clock/src/main.js
+++ pomodoro-clock/src/main.js
@@ -2,9 +2,6 @@
 
 const plusMinus = ['work-minus-button', 'work-add-button', 'break-minus-button', 'break-add-button'];
 
-let currentWork = '';
-let currentBreak = '';
-
 function changeWork() {
   $('#minutes')[0].innerHTML = $('#work-period').val();
 }
@@ -19,16 +16,30 @@ function updateSession() {
 }
 
 function changeInput() {
+  let currentWork = Number($('#work-period').val());
+  let currentBreak = Number($('#break-period').val());
+
   for (let i = 0; i < plusMinus.length; i += 1) {
     $(`#${plusMinus[i]}`).on('click', (e) => {
       if (e.target.id === 'work-minus-button') {
-        // do something
+        currentWork -= 1;
+        $('#work-period').val(currentWork);
+        changeWork();
+        // Do not allow value to go below 1
+
       } else if (e.target.id === 'work-add-button') {
-        // do something
+        currentWork += 1;
+        $('#work-period').val(currentWork);
+        changeWork();
       } else if (e.target.id === 'break-minus-button') {
-        // do something
-      } else if (e.target.id === 'work-add-button') {
-        // do something
+        currentBreak -= 1;
+        $('#break-period').val(currentBreak);
+        changeBreak();
+        // Do not allow value to go below 1
+      } else if (e.target.id === 'break-add-button') {
+        currentBreak += 1;
+        $('#break-period').val(currentBreak);
+        changeBreak();
       }
     })
   }
