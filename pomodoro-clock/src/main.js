97285c60aa3139faa2a41ebe95a1ebaf1a710228
ziggysauce <dtn3423@gmail.com>
2017-12-10 18:08:14 -0500

    input functionality

--- pomodoro-clock/src/main.js
+++ pomodoro-clock/src/main.js
@@ -1,41 +1,58 @@
 // Javascript file for calculator app
 
+// Store ids for +/- buttons
 const plusMinus = ['work-minus-button', 'work-add-button', 'break-minus-button', 'break-add-button'];
 
+// Set timer based on work period input
 function changeWork() {
-  $('#minutes')[0].innerHTML = $('#work-period').val();
+  if ($('#work-period').val() > 0) {
+    $('#minutes')[0].innerHTML = $('#work-period').val();
+  }
 }
 
+// Set timer based on break period input
 function changeBreak() {
-  $('#minutes')[0].innerHTML = $('#break-period').val();
+  if ($('#break-period').val() > 0) {
+    $('#minutes')[0].innerHTML = $('#break-period').val();
+  }
 }
 
+// Update work and break period sessions when input is changed
 function updateSession() {
   $('#work-period').on('change', () => { changeWork(); })
+    .on('keypress', (e) => {
+      if (e.keyCode === 13) {
+        changeWork();
+        return false;
+      }
+    });
   $('#break-period').on('change', () => { changeBreak(); })
+  .on('keypress', (e) => {
+    if (e.keyCode === 13) {
+      changeBreak();
+      return false;
+    }
+  });
 }
 
+// Update work and break period sessions when +/- buttons are pressed
 function changeInput() {
-  let currentWork = Number($('#work-period').val());
-  let currentBreak = Number($('#break-period').val());
-
   for (let i = 0; i < plusMinus.length; i += 1) {
     $(`#${plusMinus[i]}`).on('click', (e) => {
-      if (e.target.id === 'work-minus-button') {
+      let currentWork = Number($('#work-period').val());
+      let currentBreak = Number($('#break-period').val());
+      if (e.target.id === 'work-minus-button' && currentWork > 1) {
         currentWork -= 1;
         $('#work-period').val(currentWork);
         changeWork();
-        // Do not allow value to go below 1
-
       } else if (e.target.id === 'work-add-button') {
         currentWork += 1;
         $('#work-period').val(currentWork);
         changeWork();
-      } else if (e.target.id === 'break-minus-button') {
+      } else if (e.target.id === 'break-minus-button' && currentBreak > 1) {
         currentBreak -= 1;
         $('#break-period').val(currentBreak);
         changeBreak();
-        // Do not allow value to go below 1
       } else if (e.target.id === 'break-add-button') {
         currentBreak += 1;
         $('#break-period').val(currentBreak);
