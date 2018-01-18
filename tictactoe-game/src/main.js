65f8d2fd8f294114f3f18e3736f05a3df8c1fa1a
ziggysauce <dtn3423@gmail.com>
2018-01-18 12:23:31 -0500

    fully functional and meets all requirements

--- tictactoe-game/src/main.js
+++ tictactoe-game/src/main.js
@@ -18,6 +18,7 @@ const winningArr = [
   [0,4,8],
   [2,4,6]
 ];
+let possibleArr = [];
 
 function restartGame() {
   $('#end-game-button').on('click', (e) => {
@@ -31,13 +32,16 @@ function restartGame() {
 // If winning line doesn't equal 3 or -3 then it's a tie
 function checkGame() {
   winningArr.map((i) => {
-    if (arr[i[0]] + arr[i[1]] + arr[i[2]] === 3) {
+    const winRow = arr[i[0]] + arr[i[1]] + arr[i[2]];
+    if (winRow === 3) {
       $('.end-game-title')[0].innerHTML = 'You won!';
       $('.end-game-container').css('display', 'flex').hide().fadeIn(500);
-    } else if (arr[i[0]] + arr[i[1]] + arr[i[2]] === -3) {
+      i.preventDefault();
+    } else if (winRow === -3) {
       $('.end-game-title')[0].innerHTML = 'You lose!';
       $('.end-game-container').css('display', 'flex').hide().fadeIn(500);
-    } else if (spaces === 9) {
+      i.preventDefault();
+    } else if (spaces === 9 && winRow !== 3 && winRow !== -3) {
       $('.end-game-title')[0].innerHTML = 'It\'s a tie!';
       $('.end-game-container').css('display', 'flex').hide().fadeIn(500);
     }
@@ -58,54 +62,111 @@ function hoverSquare() {
   }
 }
 
+// Computer's turn
+function compTurn() {
+  // Find all empty space
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      possibleArr.push(i);
+    }
+  }
+
+  // Go through each empty space and see if it's a winning space
+  for (let k = 0; k < possibleArr.length; k++) {
+    let button = $(`#button_${possibleArr[k]}`)[0];
+
+    for (let j = 0; j < winningArr.length; j++) {
+      arr[possibleArr[k]] -= 1;
+
+      // console.log('possible array: ', possibleArr);
+      // console.log('comp using this space: ', possibleArr[k]);
+      // console.log('value of space used: ', arr[possibleArr[k]]);
+      // console.log(arr[winningArr[j][0]] + arr[winningArr[j][1]] + arr[winningArr[j][2]]);
+      // console.log(winningArr[j]);
+      // console.log(arr[winningArr[j][0]] + ":" + arr[winningArr[j][1]] + ":" + arr[winningArr[j][2]]);
+
+      if (arr[winningArr[j][0]] + arr[winningArr[j][1]] + arr[winningArr[j][2]] === -3) {
+        // console.log('theres a winning play');
+        $(button).addClass('active-button-second pressed');
+        $(button).attr('disabled', true);
+        button.innerHTML = comp;
+        firstPlayer = true;
+        spaces++;
+        checkGame();
+      } else {
+        // console.log('theres no winning play yet');
+        arr[possibleArr[k]] = 0;
+      }
+    }
+  }
+
+  const rand = possibleArr[Math.floor(Math.random()*possibleArr.length)];
+  const button = $(`#button_${rand}`)[0];
+  // console.log(rand);
+  // console.log(button);
+
+  arr[rand] -= 1;
+  $(button).addClass('active-button-second pressed');
+  $(button).attr('disabled', true);
+  button.innerHTML = comp;
+  firstPlayer = true;
+  spaces++;
+
+  // possibleArr = [];
+  checkGame();
+}
+
 // Click event for each square
 // Indicate which box has already been clicked
 // Do not allow same box to be clicked twice
 // Store user's turn
-// Switch to next plyer's turn
+// Switch to next player's turn
 // Update arr to check for winner
 function clickSquare() {
   for (let i = 0; i <= 8; i += 1) {
     $(`#button_${i}`).on('click', (e) => {
-      if (firstPlayer) {
-        $(e.target).addClass('active-button-first pressed');
-        $(e.target).attr('disabled', true);
-        e.target.innerHTML = human;
-        firstPlayer = false;
-        arr[i] += 1;
-        spaces++;
-      } else {
-        $(e.target).addClass('active-button-second pressed');
-        $(e.target).attr('disabled', true);
-        e.target.innerHTML = comp;
-        firstPlayer = true;
-        arr[i] -= 1;
-        spaces++;
-      }
+      $(e.target).addClass('active-button-first pressed');
+      $(e.target).attr('disabled', true);
+      e.target.innerHTML = human;
+      firstPlayer = false;
+      arr[i] += 1;
+      spaces++;
       checkGame();
+      possibleArr = [];
+      compTurn();
+      // else {
+      //   $(e.target).addClass('active-button-second pressed');
+      //   $(e.target).attr('disabled', true);
+      //   e.target.innerHTML = comp;
+      //   firstPlayer = true;
+      //   arr[i] -= 1;
+      //   spaces++;
+      // }
     });
   }
 }
 
 // User selects either X or O as their marker
-// Box disappears after selection
+// Container disappears after selection
 function selectIcon() {
   $('#select-x-user').on('click', (e) => {
     human = 'X';
     comp = 'O';
     $('#start-game').fadeOut(500);
+    $('button').attr('disabled', false);
   });
   $('#select-o-user').on('click', (e) => {
     human = 'O';
     comp = 'X';
     $('#start-game').fadeOut(500);
+    $('button').attr('disabled', false);
   });
 }
 
 
 $(document).ready(() => {
+  $('button').attr('disabled', true);
   restartGame();
-  $('#end-game').hide();
   hoverSquare();
   clickSquare();
   selectIcon();
