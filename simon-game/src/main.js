010dfeb49defbcaa97b781c70b309d88a43317b8
ziggysauce <dtn3423@gmail.com>
2018-01-25 01:26:59 -0500

    setup array, color button event handlers, cycle turns

--- simon-game/src/main.js
+++ simon-game/src/main.js
@@ -3,6 +3,7 @@ let counter = 0;
 let simonArr = [];
 let userArr = [];
 let strict = false;
+let userClick = false;
 
 const sounds = {
   greenSound: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
@@ -11,45 +12,52 @@ const sounds = {
   blueSound: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
 };
 
+const countDisplay = $('.game-counter');
+const colors = ['green', 'red', 'yellow', 'blue'];
+const green = $('#green-button');
+const red = $('#red-button');
+const yellow = $('#yellow-button');
+const blue = $('#blue-button');
+
+
+
 // Restart game function
 // Clear history
 
 // Start game function
 // Set counter to 0
+// Clear arrays
 function startGame() {
   const start = $('#start-restart-button');
-  const countDisplay = $('.game-counter');
 
   start.on('click', () => {
     start[0].innerHTML = 'Restart';
     counter = 0;
     countDisplay[0].innerHTML = counter;
-    beginSound();
+    simonArr = [];
+    userArr = [];
+    console.log('i SHOULD erase arrs');
+    $(green[0]).attr('disabled', true);
+    $(red[0]).attr('disabled', true);
+    $(yellow[0]).attr('disabled', true);
+    $(blue[0]).attr('disabled', true);
+    setTimeout(beginSound, 1000);
   });
 }
 
-// Show button presses w/ sound
-// Create array to hold sequence of pattern
-function beginSound() {
-  const green = $('#green-button')[0];
-  const red = $('#red-button')[0];
-  const yellow = $('#yellow-button')[0];
-  const blue = $('#blue-button')[0];
-
-  let random = Math.floor(Math.random() * 4);
-  simonArr.push(random);
-  console.log(simonArr);
-  if (random === 0) {
-    $(green).addClass('green-button-hover');
+// Add hover classes
+function addHover(choice) {
+  if (choice === 0) {
+    $(green[0]).addClass('green-button-hover');
     sounds.greenSound.play();
-  } else if (random === 1) {
-    $(red).addClass('red-button-hover');
+  } else if (choice === 1) {
+    $(red[0]).addClass('red-button-hover');
     sounds.redSound.play();
-  } else if (random === 2) {
-    $(yellow).addClass('yellow-button-hover');
+  } else if (choice === 2) {
+    $(yellow[0]).addClass('yellow-button-hover');
     sounds.yellowSound.play();
   } else {
-    $(blue).addClass('blue-button-hover');
+    $(blue[0]).addClass('blue-button-hover');
     sounds.blueSound.play();
   }
 
@@ -58,22 +66,58 @@ function beginSound() {
 
 // Remove hover classes
 function removeHover() {
-  const colors = ['green', 'red', 'yellow', 'blue'];
-
-  for (let i = 0; i < colors.length; i+= 1) {
+  for (let i = 0; i < colors.length; i += 1) {
     if ($(`#${colors[i]}-button`)[0].className === `color-button ${colors[i]}-button-hover`) {
       $($(`#${colors[i]}-button`)[0]).removeClass(`${colors[i]}-button-hover`);
-      console.log('removed');
+      countDisplay[0].innerHTML = counter;
+      if (userClick) {
+        console.log('its user turn');
+        $(green[0]).attr('disabled', false);
+        $(red[0]).attr('disabled', false);
+        $(yellow[0]).attr('disabled', false);
+        $(blue[0]).attr('disabled', false);
+        userTurn();
+      } else if (!userClick) {
+        // checkUser();
+        $(green[0]).attr('disabled', true);
+        $(red[0]).attr('disabled', true);
+        $(yellow[0]).attr('disabled', true);
+        $(blue[0]).attr('disabled', true);
+        console.log('its simons turn');
+        setTimeout(beginSound, 1000);
+      }
     }
   }
 }
 
-// Let user attempt to press the same buttons w/ sound
-// Create array to hold user's button presses
+// Show button presses w/ sound
+// Create array to hold sequence of pattern
+function beginSound() {
+  let simonChoice = Math.floor(Math.random() * 4);
+  simonArr.push(simonChoice);
+  counter += 1;
+  userClick = true;
+  addHover(simonChoice);
+}
 
 // Check to see if user's input matches correct pattern
 // If correct, continue game
 // If incorrect, indicate that and show sequence again; allow player one more attempt
+function checkUser() {
+  // console.log('comp arr: ', simonArr);
+  // console.log('user arr: ', userArr);
+  console.log('checkuser func reached');
+  removeHover();
+}
+
+// Let user attempt to press the same buttons w/ sound
+// Create array to hold user's button presses
+function userTurn() {
+  userClick = false;
+
+}
+
+
 
 // If user counter reaches 20, game is over and player wins
 
@@ -93,7 +137,40 @@ function strictMode() {
   });
 }
 
+//Button event handlers
+green.on('click', (e) => {
+  userArr.push(0);
+  console.log('simon: ', simonArr);
+  console.log('user: ', userArr);
+  addHover(0);
+});
+
+red.on('click', (e) => {
+  userArr.push(1);
+  console.log('simon: ', simonArr);
+  console.log('user: ', userArr);
+  addHover(1);
+});
+
+yellow.on('click', (e) => {
+  userArr.push(2);
+  console.log('simon: ', simonArr);
+  console.log('user: ', userArr);
+  addHover(2);
+});
+
+blue.on('click', (e) => {
+  userArr.push(3);
+  console.log('simon: ', simonArr);
+  console.log('user: ', userArr);
+  addHover(3);
+});
+
 $(document).ready(() => {
   startGame();
   strictMode();
+  $(green[0]).attr('disabled', true);
+  $(red[0]).attr('disabled', true);
+  $(yellow[0]).attr('disabled', true);
+  $(blue[0]).attr('disabled', true);
 });
