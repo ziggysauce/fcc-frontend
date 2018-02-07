a7d09f3f4d912f32cf03210a663a29ab05be5dc0
ziggysauce <dtn3423@gmail.com>
2018-02-07 01:04:52 -0500

    Refactor functionality

--- simon-game/src/main.js
+++ simon-game/src/main.js
@@ -1,176 +1,133 @@
-// Store variables
-let counter = 0;
-let simonArr = [];
-let userArr = [];
-let strict = false;
-let userClick = false;
-
-const sounds = {
-  greenSound: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
-  redSound: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
-  yellowSound: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
-  blueSound: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
-};
+const game = {
+  count: 0,
+  colors: ['.green-button','.red-button', '.yellow-button', '.blue-button'],
+  currentGame: [],
+  player: [],
+  sound:{
+    blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'), 
+    red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'), 
+    yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'), 
+    green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+  },
+  strict: false,
+}
 
 const countDisplay = $('.game-counter');
-const colors = ['green', 'red', 'yellow', 'blue'];
-const green = $('#green-button');
-const red = $('#red-button');
-const yellow = $('#yellow-button');
-const blue = $('#blue-button');
 
-
-
-// Restart game function
 // Clear history
-
-// Start game function
-// Set counter to 0
-// Clear arrays
-function startGame() {
-  const start = $('#start-restart-button');
-
-  start.on('click', () => {
-    start[0].innerHTML = 'Restart';
-    counter = 0;
-    countDisplay[0].innerHTML = counter;
-    simonArr = [];
-    userArr = [];
-    console.log('i SHOULD erase arrs');
-    $(green[0]).attr('disabled', true);
-    $(red[0]).attr('disabled', true);
-    $(yellow[0]).attr('disabled', true);
-    $(blue[0]).attr('disabled', true);
-    setTimeout(beginSound, 1000);
-  });
+// Start new game
+function newGame() {
+  game.currentGame = [];
+  game.count = 0;
+  addCount();
 }
-
-// Add hover classes
-function addHover(choice) {
-  if (choice === 0) {
-    $(green[0]).addClass('green-button-hover');
-    sounds.greenSound.play();
-  } else if (choice === 1) {
-    $(red[0]).addClass('red-button-hover');
-    sounds.redSound.play();
-  } else if (choice === 2) {
-    $(yellow[0]).addClass('yellow-button-hover');
-    sounds.yellowSound.play();
-  } else {
-    $(blue[0]).addClass('blue-button-hover');
-    sounds.blueSound.play();
-  }
-
-  setTimeout(removeHover, 500);
-}
-
-// Remove hover classes
-function removeHover() {
-  for (let i = 0; i < colors.length; i += 1) {
-    if ($(`#${colors[i]}-button`)[0].className === `color-button ${colors[i]}-button-hover`) {
-      $($(`#${colors[i]}-button`)[0]).removeClass(`${colors[i]}-button-hover`);
-      countDisplay[0].innerHTML = counter;
-      if (userClick) {
-        console.log('its user turn');
-        $(green[0]).attr('disabled', false);
-        $(red[0]).attr('disabled', false);
-        $(yellow[0]).attr('disabled', false);
-        $(blue[0]).attr('disabled', false);
-        userTurn();
-      } else if (!userClick) {
-        // checkUser();
-        $(green[0]).attr('disabled', true);
-        $(red[0]).attr('disabled', true);
-        $(yellow[0]).attr('disabled', true);
-        $(blue[0]).attr('disabled', true);
-        console.log('its simons turn');
-        setTimeout(beginSound, 1000);
-      }
-    }
-  }
-}
-
-// Show button presses w/ sound
-// Create array to hold sequence of pattern
-function beginSound() {
-  let simonChoice = Math.floor(Math.random() * 4);
-  simonArr.push(simonChoice);
-  counter += 1;
-  userClick = true;
-  addHover(simonChoice);
-}
-
-// Check to see if user's input matches correct pattern
-// If correct, continue game
-// If incorrect, indicate that and show sequence again; allow player one more attempt
-function checkUser() {
-  // console.log('comp arr: ', simonArr);
-  // console.log('user arr: ', userArr);
-  console.log('checkuser func reached');
-  removeHover();
-}
-
-// Let user attempt to press the same buttons w/ sound
-// Create array to hold user's button presses
-function userTurn() {
-  userClick = false;
-
-}
-
-
-
-// If user counter reaches 20, game is over and player wins
 
 // Strict mode
 // Green = Active; Red = Inactive
 // If user gets one wrong, game starts over
-function strictMode() {
+(function strictMode() {
   $('#strict-button').on('click', () => {
     const indicator = $('#strict-indicator')[0];
     if (indicator.style.background === 'red' || indicator.style.background === '') {
       indicator.style.background = 'green';
-      strict = true;
+      game.strict = true;
     } else {
       indicator.style.background = 'red';
-      strict = false;
+      game.strict = false;
     }
+    newGame();
   });
+}());
+
+// Track count for each turn
+function showMoves() {
+  let i = 0;
+  const move = setInterval(() => {
+    playGame(game.currentGame[i]);
+    i += 1;
+    if (i >= game.currentGame.length) {
+      clearInterval(move);
+    }
+  }, 500)
+  
+  game.player = [];
 }
 
-//Button event handlers
-green.on('click', (e) => {
-  userArr.push(0);
-  console.log('simon: ', simonArr);
-  console.log('user: ', userArr);
-  addHover(0);
-});
+// Play sounds associated with each color
+function sound(color) {
+  switch(color) {
+    case'.green-button':
+      game.sound.green.play();
+      break;
+    case '.blue-button':
+      game.sound.blue.play();
+      break;
+    case '.red-button':
+      game.sound.red.play();
+      break;
+    case '.dark-button':
+      game.sound.yellow.play();
+      break;
+  };
+}
 
-red.on('click', (e) => {
-  userArr.push(1);
-  console.log('simon: ', simonArr);
-  console.log('user: ', userArr);
-  addHover(1);
-});
+function playGame(color) {
+  $(color).addClass('hover');
+  console.log(color);
+  sound(color);
+  setTimeout(() => {
+    $(color).removeClass('hover');
+  }, 300);
+}
 
-yellow.on('click', (e) => {
-  userArr.push(2);
-  console.log('simon: ', simonArr);
-  console.log('user: ', userArr);
-  addHover(2);
-});
+function addToPlayer(id) {
+  let input = "." + id + "-button";
+  $(input).addClass('hover');
+  sound(input);
+  setTimeout(() => {
+    $(input).removeClass('hover');
+  }, 300);
+  game.player.push(input);
+  playerTurn(input);
+}
 
-blue.on('click', (e) => {
-  userArr.push(3);
-  console.log('simon: ', simonArr);
-  console.log('user: ', userArr);
-  addHover(3);
-});
+function playerTurn(e) {
+  if (game.player[game.player.length - 1] !== game.currentGame[game.player.length - 1]) {
+    if (game.strict){
+      alert('Game over!');
+      newGame();
+    } else {
+      alert('Incorrect! Try again!');
+      showMoves();
+    }
+  } else {
+    sound(e);
+    let check = game.player.length === game.currentGame.length;
+    if (check) {
+      if (game.count == 20) {
+        alert('You win!');
+      } else {
+        // alert('Next round!');
+        addCount();
+      }
+    }
+  }
+} 
 
-$(document).ready(() => {
-  startGame();
-  strictMode();
-  $(green[0]).attr('disabled', true);
-  $(red[0]).attr('disabled', true);
-  $(yellow[0]).attr('disabled', true);
-  $(blue[0]).attr('disabled', true);
+function generateMove(){
+  game.currentGame.push(game.colors[(Math.floor(Math.random()*4))]);
+  showMoves();
+}
+
+function addCount() {
+  game.count++;
+  countDisplay[0].innerHTML = game.count;
+  generateMove();
+}
+
+// newGame();
+
+const start = $('#start-restart-button');
+start.on('click', () => {
+  newGame();
 });
